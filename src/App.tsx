cd94d6f2ee95b680da02cb13bb41c73ac46e1c75
Mateo <mtaycruz@gmail.com>
2023-01-23 21:36:23 -0500

    stup fetch requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  interface Race {
+    slug: string;
+    name: string;
+  }
+
+  interface RaceResults{
+    results: Race[];
+  }
+
+  const [races, setRaces] = useState<Race[]>([])
+
+  useEffect(() => {  
+    const races = async() => {
+      try{
+        const response = await fetch('https://api.open5e.com/races/')
+        const data: RaceResults = await response.json()
+        setRaces(data.results)
+        
+      } catch (err){
+          console.log(err)
+      }
+
+  }
+    races()
+  }, [])
+
+
+  // console.log(race.name)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {races.map((race) => 
+        <div key={race.slug}> {race.name} </div>
+      )
+      }
+      
     </div>
   );
 }
